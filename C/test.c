520688f2a8fb2370af7601de151b93cb481514dc
MexicanBauss <edu98cantos@gmail.com>
2024-05-09 20:04:20 +0200

    Made a recursive factorial :O :I -_-

--- C/test.c
+++ C/test.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    for (int i = 1; i<argc; i++) {
-        printf("%d: %s\n", i, argv[i]);
+long factorial(long n) {
+    if (n<=0) {
+        return 1;
     };
-    return 0;
+
+    return n * factorial(n-1);
+};
+
+int main() {
+    long j = 0;
+        while(j<21){
+        long facto = factorial(j);
+            printf("Factorial %ld!:\t%ld\n", j, facto);
+        j++;
+    }
+    printf("\n");
+    return 0; 
 }
 
